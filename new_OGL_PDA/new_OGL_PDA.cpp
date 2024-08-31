cd63d80252b2063792f514cd90d5941329622e2a
Dmitriy <60214129a@gmail.com>
2024-08-31 19:51:58 +0300

    making changes according to attempt 3, in order to solve the problem of attempt 2, add changes if delta x is less than delta y, then change the construction method from x+=1 to y=+1.

--- new_OGL_PDA/new_OGL_PDA.cpp
+++ new_OGL_PDA/new_OGL_PDA.cpp
@@ -4,16 +4,30 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 
 void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
-    for (int x = x0; x <= x1; x+=1) {
+    bool steep = false;
+    if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0 > x1) {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    for (int x = x0; x <= x1; x++) {
         float t = (x - x0) / (float)(x1 - x0);
         int y = y0 * (1. - t) + y1 * t;
-        image.set(x, y, color);
+        if (steep) {
+            image.set(y, x, color);
+        }
+        else {
+            image.set(x, y, color);
+        }
     }
 }
 
 int main(int argc, char** argv) {
     TGAImage image(100, 100, TGAImage::RGB);
-    image.set(52, 41, red);
     line(13, 20, 80, 40, image, white);
     line(20, 13, 40, 80, image, red);
     line(80, 40, 13, 20, image, red);
